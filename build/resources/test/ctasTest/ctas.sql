5a5360976975bca066f15db399a8c41dc2e1c560
Patel Himanshu <patel_himanshu_3@cat.com>
2015-12-08 20:11:08 -0600

    Corrected Version…

--- build/resources/test/ctasTest/ctas.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL TABLE foo (s1 string, s2 string)
-  ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-  STORED AS TEXTFILE
-  LOCATION '${hiveconf:hive.vs.uri}/foo/';
-
-
-CREATE TABLE foo_prim as select * from foo;
-
-
